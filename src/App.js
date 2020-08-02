233dae261a2b8ab794b4b6827ced6d00e5ed9c0c
CastalJaemin <ehdrud1332@gmail.com>
2020-08-02 10:53:05 +0900

    add UploadForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './Components/Title';
+import UploadForm from './Components/uploadForm';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UploadForm />
     </div>
   );
 }
